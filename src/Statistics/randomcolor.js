ae42a7b420012b3f7d733081f634b125e20392ca
Kirill Filonchuk <kir.realtor@gmail.com>
2021-08-24 22:53:54 +0300

    plus finish

--- src/Statistics/randomcolor.js
+++ src/Statistics/randomcolor.js
@@ -1,9 +1,11 @@
 function randomColor(min = 1, max = 230) {
-  let r = (Math.floor(Math.random() * (max - min) + 1) + min).toString(16);
-  let g = (Math.floor(Math.random() * (max - min) + 1) + min).toString(16);
-  let b = (Math.floor(Math.random() * (max - min) + 1) + min).toString(16);
+  let r = (Math.floor(Math.random() * (max - min) + 1) + 10).toString();
+  let g = (Math.floor(Math.random() * (max - min) + 1) + 10).toString();
+  let b = (Math.floor(Math.random() * (max - min) + 1) + 10).toString();
 
-  let color = '#' + r + g + b;
+  // let colorHex = `#${r}${g}${b}`;
+  let color = `rgb(${r},${g},${b})`;
+
   return color;
 }
 
