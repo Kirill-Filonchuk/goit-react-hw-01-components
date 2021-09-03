099a12c638e68d36c59453d3521cff7a5a58eae0
Kirill Filonchuk <kir.realtor@gmail.com>
2021-09-03 20:39:54 +0300

    fixed

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,12 +1,27 @@
+import PropTypes from 'prop-types';
+
 import FriendListItem from './FriendListItem/FriendListItem';
 import s from './Friends.module.css';
 
 function FriendList({ friends }) {
   return (
     <ul className={s.friendList}>
-      <FriendListItem friends={friends} />
+      {friends.map(friend => (
+        <FriendListItem {...friend} key={friend.id} />
+      ))}
     </ul>
   );
 }
 
 export default FriendList;
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+      isOnline: PropTypes.bool,
+      id: PropTypes.number,
+    }).isRequired,
+  ).isRequired,
+};
--- src/components/FriendList/FriendListItem/FriendListItem.js
+++ src/components/FriendList/FriendListItem/FriendListItem.js
@@ -1,25 +1,23 @@
-import PropTypes from 'prop-types';
 import s from '../Friends.module.css';
 
-function FriendListItem({ friends }) {
-  return friends.map(friend => (
-    <li className={s.item} key={friend.id}>
-      {friend.isOnline ? <span className={s.online}></span> : <span className={s.offline}></span>}
-      <img className={s.avatar} src={friend.avatar} alt={friend.name} width="48" />
-      <p className={s.name}>{friend.name}</p>
+function FriendListItem({ avatar, name, isOnline }) {
+  return (
+    <li className={s.item}>
+      {isOnline ? <span className={s.online}></span> : <span className={s.offline}></span>}
+      <img className={s.avatar} src={avatar} alt={name} width="48" />
+      <p className={s.name}>{name}</p>
     </li>
-  ));
+  );
 }
 
-FriendListItem.propTypes = {
-  friends: PropTypes.arrayOf(
-    PropTypes.shape({
-      avatar: PropTypes.string,
-      name: PropTypes.string,
-      isOnline: PropTypes.bool,
-      id: PropTypes.number,
-    }).isRequired,
-  ).isRequired,
-};
-
 export default FriendListItem;
+
+// function FriendListItem({ friends }) {
+//   return friends.map(friend => (
+//     <li className={s.item} key={friend.id}>
+//       {friend.isOnline ? <span className={s.online}></span> : <span className={s.offline}></span>}
+//       <img className={s.avatar} src={friend.avatar} alt={friend.name} width="48" />
+//       <p className={s.name}>{friend.name}</p>
+//     </li>
+//   ));
+// }
